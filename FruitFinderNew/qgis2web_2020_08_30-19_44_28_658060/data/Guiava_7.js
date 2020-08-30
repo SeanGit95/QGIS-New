a187cc44e617bb2d3c8dc4024e25e398ab12df95
SeanGit95 <65398393+SeanGit95@users.noreply.github.com>
2020-08-30 19:52:28 +0300

    added the project

---
+++ FruitFinderNew/qgis2web_2020_08_30-19_44_28_658060/data/Guiava_7.js
@@ -0,0 +1,12 @@
+var json_Guiava_7 = {
+"type": "FeatureCollection",
+"name": "Guiava_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "23", "Name": "פראנצויז 5", "description": "גויאבה בחצר של הבנין. מרגיש לא הכי לגיטימי אבל משיחה עם הדיירים נראה שאף אחד לא קוטף את זה‎‎", "layer": "עצי פרי בתל אביב יפו أشجار فاكهة في تل أبيب يافا Fruit trees in Tel Aviv Yafo בננה, פפאיה, גויאבה, אבוקדו موز, ببايا, جوافة, أفوكادو Banana, Papaya, Guava, Avocado", "xcoord": 3869841.9824920525, "ycoord": 3770640.8900128859, "zcoord": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 34.763382, 32.055826 ] } },
+{ "type": "Feature", "properties": { "fid": "26", "Name": "ביאליק 10", "description": "גויאבה לא ממש לגיטימית", "layer": "עצי פרי בתל אביב יפו أشجار فاكهة في تل أبيب يافا Fruit trees in Tel Aviv Yafo בננה, פפאיה, גויאבה, אבוקדו موز, ببايا, جوافة, أفوكادو Banana, Papaya, Guava, Avocado", "xcoord": 3870621.2300595543, "ycoord": 3772723.5807861257, "zcoord": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 34.7703821, 32.0716812 ] } },
+{ "type": "Feature", "properties": { "fid": "27", "Name": "ביאליק 25", "description": "גויאבה תותית של עץבעיר. מרגיש לגיטימי", "layer": "עצי פרי בתל אביב יפו أشجار فاكهة في تل أبيب يافا Fruit trees in Tel Aviv Yafo בננה, פפאיה, גויאבה, אבוקדו موز, ببايا, جوافة, أفوكادو Banana, Papaya, Guava, Avocado", "xcoord": 3870673.1606020094, "ycoord": 3772936.241737959, "zcoord": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 34.7708486, 32.0733 ] } },
+{ "type": "Feature", "properties": { "fid": "42", "Name": "טשרניחובסקי 31", "description": "גויאבה חצי לגיטימית", "layer": "עצי פרי בתל אביב יפו أشجار فاكهة في تل أبيب يافا Fruit trees in Tel Aviv Yafo בננה, פפאיה, גויאבה, אבוקדו موز, ببايا, جوافة, أفوكادو Banana, Papaya, Guava, Avocado", "xcoord": 3870760.8135690596, "ycoord": 3772899.0244858922, "zcoord": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 34.771636, 32.0730167 ] } },
+{ "type": "Feature", "properties": { "fid": "50", "Name": "אלכסנדר פן 4", "description": "גויאבה חצי לגיטימית", "layer": "עצי פרי בתל אביב יפו أشجار فاكهة في تل أبيب يافا Fruit trees in Tel Aviv Yafo בננה, פפאיה, גויאבה, אבוקדו موز, ببايا, جوافة, أفوكادو Banana, Papaya, Guava, Avocado", "xcoord": 3875406.5210102848, "ycoord": 3779451.1039053053, "zcoord": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 34.8133691, 32.122878 ] } }
+]
+}
---
+++ FruitFinderNew/qgis2web_2020_08_30-19_44_28_658060/data/Sabres_16.js
@@ -0,0 +1,12 @@
+var json_Sabres_16 = {
+"type": "FeatureCollection",
+"name": "Sabres_16",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "147", "Name": "רייך 8", "description": "בין הבניינים סברס חצי לגיטימי", "layer": "עצי פרי בתל אביב יפו أشجار فاكهة في تل أبيب يافا Fruit trees in Tel Aviv Yafo חושחש, שקמה, חרוב نارنج, جميز, خروب Bitter Orange, Sycamore Fig, Carob", "xcoord": 3871514.1348271556, "ycoord": 3773671.7560695754, "zcoord": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 34.7784032, 32.0788986 ] } },
+{ "type": "Feature", "properties": { "fid": "152", "Name": "פטאי 21", "description": "סברס, מרגיש לגיטימי", "layer": "עצי פרי בתל אביב יפו أشجار فاكهة في تل أبيب يافا Fruit trees in Tel Aviv Yafo חושחש, שקמה, חרוב نارنج, جميز, خروب Bitter Orange, Sycamore Fig, Carob", "xcoord": 3873935.1890365719, "ycoord": 3777473.1570560867, "zcoord": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 34.8001519, 32.1078287 ] } },
+{ "type": "Feature", "properties": { "fid": "157", "Name": "סלמה 171", "description": "סברס חצי לגיטימי", "layer": "עצי פרי בתל אביב יפו أشجار فاكهة في تل أبيب يافا Fruit trees in Tel Aviv Yafo חושחש, שקמה, חרוב نارنج, جميز, خروب Bitter Orange, Sycamore Fig, Carob", "xcoord": 3871797.932736984, "ycoord": 3770381.5247143237, "zcoord": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 34.7809526, 32.0538513 ] } },
+{ "type": "Feature", "properties": { "fid": "158", "Name": "רפידים, צפונית לחניה", "description": "סברס לגיטימי", "layer": "עצי פרי בתל אביב יפו أشجار فاكهة في تل أبيب يافا Fruit trees in Tel Aviv Yafo חושחש, שקמה, חרוב نارنج, جميز, خروب Bitter Orange, Sycamore Fig, Carob", "xcoord": 3875284.4814525279, "ycoord": 3777766.3593722582, "zcoord": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 34.8122728, 32.1100597 ] } },
+{ "type": "Feature", "properties": { "fid": "160", "Name": "כל ישראל חברים 10", "description": "סברס לגיטימי", "layer": "עצי פרי בתל אביב יפו أشجار فاكهة في تل أبيب يافا Fruit trees in Tel Aviv Yafo חושחש, שקמה, חרוב نارنج, جميز, خروب Bitter Orange, Sycamore Fig, Carob", "xcoord": 3869884.6178570264, "ycoord": 3771233.6803265894, "zcoord": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 34.763765, 32.0603391 ] } }
+]
+}
