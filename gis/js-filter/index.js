b12cbcadbf148e634e3bc6602dd3f793bdb6e160
adirbublil <66596989+adirbublil@users.noreply.github.com>
2020-10-14 16:59:11 +0300

    fix filter

--- gis/js-filter/index.js
+++ gis/js-filter/index.js
@@ -3,60 +3,56 @@
 window.onload = function () {
     let options = new Set();
     let layers = [];
-    let selected = '';
+    let selectedType = '';
+    let selectedLegit = '';
+    
     let selectEl = document.getElementById('filterByType');
     let selectLegit = document.getElementById('filterByLegit');
-    selectLegit.addEventListener('change', function (event) {
-        selected = event.target.value;
-        if (selected == 'הכל') {
-            layers.forEach(l => {
-                map.addLayer(l)
 
-            });
-            return;
-        }
-        if (selected == 'לגיטימי') {
-            layers.forEach(l => {
+
+
+    function drwaLayers() {
+        layers.forEach(l => {
+            map.removeLayer(l);
+        });
+      
+        let finalLayer = layers.filter(l => {
+            let result = true;
+            let resultLegit = true;
+           
+            if (selectedType == 'select') {
+                result = true;
+            } else if (l.options.pane !== selectedType) {
+                result = false;
+            }
+
+            if (selectedLegit == 'לגיטימי') {
                 if (l.feature.properties.description.includes('לא לגיטימי') ||
                     l.feature.properties.description.includes('חצי לגיטימי')
                 ) {
-                    map.removeLayer(l)
-                } else {
-                    if (!map.hasLayer(l)) {
-                        map.addLayer(l)
-                    }
-                }
-            })
-            return;
-        }
-        layers.forEach(l => {
-            if (!l.feature.properties.description.includes(selected)) {
-                map.removeLayer(l)
-            } else {
-                if (!map.hasLayer(l)) {
-                    map.addLayer(l)
+                    resultLegit = false
                 }
             }
+            if (selectedLegit !== 'הכל' && !l.feature.properties.description.includes(selectedLegit)) {
+                resultLegit = false;
+            } 
+
+            return result && resultLegit;
         })
+        console.log('finalLayer',finalLayer.length);
+        finalLayer.forEach((l,i) => {
+            map.addLayer(l);
+        });
+    }
+    selectLegit.addEventListener('change', function (event) {
+        selectedLegit = event.target.value;
+        drwaLayers();
+
     })
     selectEl.addEventListener('change', function (event) {
-        selected = event.target.value;
-        if (selected == 'select') {
-            layers.forEach(l => {
-                map.addLayer(l)
+        selectedType = event.target.value;
+        drwaLayers();
 
-            });
-            return;
-        }
-        layers.forEach(l => {
-            if (l.options.pane !== selected) {
-                map.removeLayer(l)
-            } else {
-                if (!map.hasLayer(l)) {
-                    map.addLayer(l)
-                }
-            }
-        })
     })
 
     map.eachLayer(function (layer) {
@@ -68,11 +64,11 @@ window.onload = function () {
 
         }
     });
+   
     let option = document.createElement('option');
     option.innerHTML = 'select';
     selectEl.appendChild(option)
 
-    selectEl.appendChild(option)
 
     options.forEach(function (opt) {
         let option = document.createElement('option');
